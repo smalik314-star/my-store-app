ad3d59486b8e2184a75c85ae28552f250b17d620
shanu malik <smalik314@gmail.com>
2026-07-23 14:57:05 +0530

    feat: add shared ERP interface components

---
+++ src/components/erp/index.ts
@@ -0,0 +1,7 @@
+export * from './ERPToolbar';
+export * from './KeyboardShortcutHint';
+export * from './NumericInputs';
+export * from './PageHeader';
+export * from './Pagination';
+export * from './SearchInput';
+export * from './StatusBadge';
